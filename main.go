76f629c380e8850d84901b293197376f116fdc49
NodirBobiev <nodir.bobiev@gmail.com>
2024-03-26 15:13:05 +0300

    add avatar, authavatar, gravatar

--- main.go
+++ main.go
@@ -59,7 +59,7 @@ func main() {
 		),
 	)
 
-	r := newRoom()
+	r := newRoom(UseGravatar)
 	r.tracer = trace.New(os.Stdout)
 	http.Handle("/chat", MustAuth(&templateHandler{filename: "chat.html"}))
 	http.Handle("/login", &templateHandler{filename: "login.html"})
--- room.go
+++ room.go
@@ -15,6 +15,7 @@ type room struct {
 	leave   chan *client
 	clients map[*client]bool
 	tracer  trace.Tracer
+	avatar  Avatar
 }
 
 func (r *room) run() {
@@ -67,12 +68,13 @@ func (r *room) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	client.read()
 }
 
-func newRoom() *room {
+func newRoom(avatar Avatar) *room {
 	return &room{
 		forward: make(chan *message),
 		join:    make(chan *client),
 		leave:   make(chan *client),
 		clients: make(map[*client]bool),
 		tracer:  trace.Off(),
+		avatar:  avatar,
 	}
 }
